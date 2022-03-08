8cde06956960f263e78f05f020b2edd8e0acb565
HYKTSNG <Sin5hayak@gmail.com>
2022-03-08 13:14:59 +0900

    add: Dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,16 @@ class _TodoListPageState extends State<TodoListPage> {
         title: const Text('リスト一覧'),
       ),
       body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(todoList[index]),
-            ),
-          );
-        },
-      ),
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+                key: UniqueKey(),
+                child: Card(
+                  child: ListTile(
+                    title: Text(todoList[index]),
+                  ),
+                ));
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final newListText = await Navigator.of(context).push(
