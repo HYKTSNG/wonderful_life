21f86e1c177c23faf024b22d3e05c62403dc2567
HYKTSNG <Sin5hayak@gmail.com>
2022-03-10 15:12:49 +0900

    move: TodoListPage

---
+++ lib/todo_list_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'todo_add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = ["誰かに挨拶しよう!", "ゴミ拾いしよう!", "誰かに感謝を伝えよう!"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: SizedBox(
+        child: Drawer(
+            child: ListView(
+          children: const [
+            DrawerHeader(
+              child: Text(
+                "カテゴリー覧",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text("Done"),
+              trailing: Icon(Icons.star, color: Colors.green),
+            ),
+            ListTile(
+              title: Text("Garbage box"),
+              trailing: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            ListTile(),
+          ],
+        )),
+      ),
+      appBar: AppBar(
+        title: const Text('Wonderful Life'),
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+                key: UniqueKey(),
+                onDismissed: (direction) {
+                  setState(() {
+                    todoList.remove(todoList[index]);
+                  });
+                  if (direction == DismissDirection.endToStart) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("delete"),
+                      duration: Duration(seconds: 1),
+                    ));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("good job"),
+                        duration: Duration(seconds: 1)));
+                  }
+                },
+                secondaryBackground: Container(
+                  alignment: AlignmentDirectional.centerEnd,
+                  color: Colors.red,
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                background: Container(
+                  alignment: AlignmentDirectional.centerEnd,
+                  color: Colors.green,
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    child: Icon(
+                      Icons.star,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                child: Card(
+                  child: ListTile(
+                    title: Text(
+                      todoList[index],
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ));
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
