08325534a97da86f37a042d4d8364cebb7a5002b
HYKTSNG <Sin5hayak@gmail.com>
2022-03-10 16:16:30 +0900

    flutter build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "53fb59e54fb87bb44dba7c2200e1dead",
-"index.html": "683e3959773895567e44d810db331e90",
-"/": "683e3959773895567e44d810db331e90",
-"main.dart.js": "2189e2dba538cd7ad1f6f5b3af8bbde0",
+"index.html": "3d6f2ddcb23e5594cf8ea9d6293f5c51",
+"/": "3d6f2ddcb23e5594cf8ea9d6293f5c51",
+"main.dart.js": "3092550422a8f77ef10b097208259156",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
